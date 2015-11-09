775e9e3fd18a865a999981f81eceb851107323ff
julienperret <julien.perret@gmail.com>
2015-11-09 10:24:19 +0100

    Added DirectSampler interface.

--- librjmcmc4j-core/src/main/java/fr/ign/rjmcmc/sampler/Density.java
+++ librjmcmc4j-core/src/main/java/fr/ign/rjmcmc/sampler/Density.java
@@ -1,11 +1,8 @@
 package fr.ign.rjmcmc.sampler;
 
-import org.apache.commons.math3.random.RandomGenerator;
-
 import fr.ign.rjmcmc.configuration.Configuration;
 import fr.ign.rjmcmc.configuration.Modification;
 
 public interface Density<C extends Configuration<C, M>, M extends Modification<C, M>> {
 	double pdfRatio(C config, M modif);
-	void init(RandomGenerator random, C conf);
 }
