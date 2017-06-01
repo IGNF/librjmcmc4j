d4300eaaf50ac47bbe9b80b96ba126edcc669766
ilokhat <lartane@gmail.com>
2017-06-01 14:32:50 +0200

    minor formatting

--- librjmcmc4j-core/src/main/java/fr/ign/simulatedannealing/endtest/CompositeEndTest.java
+++ librjmcmc4j-core/src/main/java/fr/ign/simulatedannealing/endtest/CompositeEndTest.java
@@ -1,6 +1,5 @@
 package fr.ign.simulatedannealing.endtest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,28 +9,30 @@ import fr.ign.rjmcmc.sampler.Sampler;
 import fr.ign.simulatedannealing.temperature.Temperature;
 
 /**
- * A composite entest that takes a list of end tests and evaluates them all. 
- * It should stop when the first end test wants to stop.
+ * A composite entest that takes a list of end tests and evaluates them all. It
+ * should stop when the first end test wants to stop.
  */
 public class CompositeEndTest implements EndTest {
-	List<EndTest> tests;
-	public CompositeEndTest(EndTest... t) {
-		tests = Arrays.asList(t);
-	}
-	@Override
-	public <C extends Configuration<C, M>, M extends Modification<C, M>> boolean evaluate(C config, Sampler<C, M> sampler, Temperature t) {
-		boolean result = false;
-		for (EndTest test : tests) {
-			boolean r = test.evaluate(config, sampler, t);
-			result = result | r;
-		}
-		return result;
-	}
+  List<EndTest> tests;
 
-	@Override
-	public void stop() {
-		for (EndTest test : tests) {
-			test.stop();
-		}
-	}
+  public CompositeEndTest(EndTest... t) {
+    tests = Arrays.asList(t);
+  }
+
+  @Override
+  public <C extends Configuration<C, M>, M extends Modification<C, M>> boolean evaluate(C config, Sampler<C, M> sampler, Temperature t) {
+    boolean result = false;
+    for (EndTest test : tests) {
+      boolean r = test.evaluate(config, sampler, t);
+      result = result | r;
+    }
+    return result;
+  }
+
+  @Override
+  public void stop() {
+    for (EndTest test : tests) {
+      test.stop();
+    }
+  }
 }
